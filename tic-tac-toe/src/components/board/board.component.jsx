d6c537d8715cd919a46560b159d3a17438d2cfd8
ASofy0007 <103221068+ASofy0007@users.noreply.github.com>
2023-05-16 14:31:22 +0100

    Add files via upload

---
+++ tic-tac-toe/src/components/board/board.component.jsx
@@ -0,0 +1,22 @@
+import "./board.css"
+
+import React from "react"
+
+function Board(){
+    return(
+        <section id="board">
+              
+            <div className="board-row">
+                
+            </div>
+            <div className="board-row">
+            
+            </div>
+            <div className="board-row">
+            
+            </div>
+        </section>
+    );
+}
+
+export default Board
