daac2ab1be557915ac91ffce87b8a93b247ef4f6
ASofy0007 <103221068+ASofy0007@users.noreply.github.com>
2023-05-16 14:22:54 +0100

    Add files via upload

---
+++ tic-tac-toe/src/components/index.js
@@ -0,0 +1,3 @@
+export { default as ControlPanel } from "./control-panel/control-panel.component";
+export { default as GamePanel } from "./game-panel/game-panel.component";
+export { default as Board } from "./board/board.component";
