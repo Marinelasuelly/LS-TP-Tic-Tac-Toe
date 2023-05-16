5ebae8595a16a31d4f5546cde698c903e56e2dd6
ASofy0007 <103221068+ASofy0007@users.noreply.github.com>
2023-05-16 14:26:57 +0100

    Add files via upload

---
+++ tic-tac-toe/src/components/game-panel/game-panel.component.jsx
@@ -0,0 +1,15 @@
+import "./game-panel.css"
+
+import React from "react"
+
+function GamePanel(){
+    return(
+        <section id="game-panel">
+                
+            
+            
+        </section>
+    );
+}
+
+export default GamePanel
