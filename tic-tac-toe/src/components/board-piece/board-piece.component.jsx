fbbe026f9ad72884d6dd7c0d15213464d09a6aac
ASofy0007 <103221068+ASofy0007@users.noreply.github.com>
2023-06-06 19:47:51 +0100

    Update board-piece.component.jsx

---
+++ tic-tac-toe/src/components/board-piece/board-piece.component.jsx
@@ -0,0 +1,14 @@
+
+import "./board-piece.css"
+
+import React from "react"
+
+function BoardPiece(){
+    return(
+        <section id="boardPiece">
+              <button className="piece">X</button>
+        </section>
+    );
+}
+
+export default BoardPiece
