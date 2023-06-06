d45a56c94893317f22feb6b4c889d328c9cb3163
ASofy0007 <103221068+ASofy0007@users.noreply.github.com>
2023-06-06 20:16:34 +0100

    Update game-panel.component.jsx

--- tic-tac-toe/src/components/game-panel/game-panel.component.jsx
+++ tic-tac-toe/src/components/game-panel/game-panel.component.jsx
@@ -1,12 +1,45 @@
 import "./game-panel.css"
+import Board from "../board/board.component";
 
 import React from "react"
 
 function GamePanel(){
     return(
         <section id="game-panel">
-                
-            
+            <div className="game-row" id = "row1">
+                <div className="tab1">
+                    <Board />
+                </div>  
+                <div className="tab2">
+                    <Board />
+                </div>  
+                <div className="tab3">
+                    <Board />
+                </div>  
+            </div>
+            <div className="game-row" id = "row2">
+                <div className="tab1">
+                    <Board />
+                </div>  
+                <div className="tab2">
+                    <Board />
+                </div>  
+                <div className="tab3">
+                    <Board />
+                </div> 
+            </div>
+            <div className="game-row" id = "row3">
+                <div className="tab1">
+                    <Board />
+                </div>  
+                <div className="tab2">
+                    <Board />
+                </div>  
+                <div className="tab3">
+                    <Board />
+                </div> 
+            </div>
+              
             
         </section>
     );
