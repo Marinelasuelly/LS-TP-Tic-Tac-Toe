2461134fb8d129f81f9492f9e2ceaf9bf606d3a2
ASofy0007 <103221068+ASofy0007@users.noreply.github.com>
2023-06-06 20:23:00 +0100

    Update index.js

--- tic-tac-toe/src/components/index.js
+++ tic-tac-toe/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as ControlPanel } from "./control-panel/control-panel.component";
 export { default as GamePanel } from "./game-panel/game-panel.component";
 export { default as Board } from "./board/board.component";
+export { default as BoardPiece } from "./board-piece/board-piece.component";
