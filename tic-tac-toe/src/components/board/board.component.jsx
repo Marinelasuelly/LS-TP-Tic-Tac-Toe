052c432df9fb66727bb17abb9d33fdabbc5b212e
ASofy0007 <103221068+ASofy0007@users.noreply.github.com>
2023-06-06 20:08:55 +0100

    Update board.component.jsx

--- tic-tac-toe/src/components/board/board.component.jsx
+++ tic-tac-toe/src/components/board/board.component.jsx
@@ -1,19 +1,45 @@
 import "./board.css"
+import BoardPiece from "../board-piece/board-piece.component";
+
 
 import React from "react"
 
 function Board(){
     return(
-        <section id="board">
+        <section className="board">
               
-            <div className="board-row">
-                
+            <div className="board-row" id = "uno">
+                <div className="peca1">
+                    <BoardPiece value = ' '/>
+                </div>  
+                <div className="peca2">
+                    <BoardPiece value = ' '/>
+                </div>  
+                <div className="peca3">
+                    <BoardPiece value = ' '/>
+                </div>  
             </div>
-            <div className="board-row">
-            
+            <div className="board-row" id = "dos">
+                <div className="peca1">
+                    <BoardPiece value = ' '/>
+                </div>  
+                <div className="peca2">
+                    <BoardPiece value = ' '/>
+                </div>  
+                <div className="peca3">
+                    <BoardPiece value = ' '/>
+                </div>  
             </div>
-            <div className="board-row">
-            
+            <div className="board-row" id = "tres">
+                <div className="peca1">
+                    <BoardPiece value = ' '/>
+                </div>  
+                <div className="peca2">
+                    <BoardPiece value = ' '/>
+                </div>  
+                <div className="peca3">
+                    <BoardPiece value = ' '/>
+                </div>  
             </div>
         </section>
     );
