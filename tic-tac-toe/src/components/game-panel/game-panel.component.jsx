6eb6de1de98c99c9b01952b1d0d2977eb4f64d89
ASofy0007 <103221068+ASofy0007@users.noreply.github.com>
2023-06-09 14:54:18 +0100

    Update game-panel.component.jsx

--- tic-tac-toe/src/components/game-panel/game-panel.component.jsx
+++ tic-tac-toe/src/components/game-panel/game-panel.component.jsx
@@ -1,42 +1,60 @@
 import "./game-panel.css"
-import Board from "../board/board.component";
+import React, { useState } from "react"
 
-import React from "react"
 
-function GamePanel(){
+import { checkResult } from "../../helpers";
+import {Board} from "../../components";
+
+
+function GamePanel(){ //componete que contem os 9 tabuleiros
+
+   
+    const [result, setResult] = useState(Array(9).fill(null));
+
+    function boardResultHandler(i,value){
+        
+        const nextRes = result.slice();
+        nextRes[i] = value;
+        setResult(nextRes);
+        console.log(result);
+
+    }
+
+    checkResult(result);
+ 
     return(
         <section id="game-panel">
             <div className="game-row" id = "row1">
                 <div className="tab1">
-                    <Board />
+                    <Board letter = {boardResultHandler} i={0}/>
                 </div>  
                 <div className="tab2">
-                    <Board />
+                    <Board letter = {boardResultHandler} i={1}/>
                 </div>  
                 <div className="tab3">
-                    <Board />
+                    <Board letter = {boardResultHandler} i={2}/>
                 </div>  
             </div>
             <div className="game-row" id = "row2">
                 <div className="tab1">
-                    <Board />
+                    <Board letter = {boardResultHandler} i={3}/>
                 </div>  
                 <div className="tab2">
-                    <Board />
+                    <Board letter = {boardResultHandler} i={4}/>
                 </div>  
                 <div className="tab3">
-                    <Board />
+                    <Board letter = {boardResultHandler} i={5}/>
                 </div> 
             </div>
             <div className="game-row" id = "row3">
                 <div className="tab1">
-                    <Board />
+                    <Board letter = {boardResultHandler} i={6}/>
                 </div>  
                 <div className="tab2">
-                    <Board />
+                    <Board letter = {boardResultHandler} i={7}/>
                 </div>  
                 <div className="tab3">
-                    <Board />
+                    <Board letter = {boardResultHandler} i={8}/>
                 </div> 
             </div>
               
@@ -44,5 +62,6 @@ function GamePanel(){
         </section>
     );
 }
+
 
 export default GamePanel
