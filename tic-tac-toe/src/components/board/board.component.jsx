055ce3bb68bcee086c43ca135e6f87b5610448d6
ASofy0007 <103221068+ASofy0007@users.noreply.github.com>
2023-06-09 14:52:34 +0100

    Update board.component.jsx

--- tic-tac-toe/src/components/board/board.component.jsx
+++ tic-tac-toe/src/components/board/board.component.jsx
@@ -1,46 +1,80 @@
 import "./board.css"
-import BoardPiece from "../board-piece/board-piece.component";
+import {BoardPiece} from "../../components";
 
+import React, { useState } from "react"
+import { checkResult } from "../../helpers";
 
-import React from "react"
+function Board({letter, i}){ // componente que contem 9 pecas
+    
+    const [symbol, setSymbol] = useState("X");
+    const [piece, setPiece] = useState(Array(9).fill(null))//array de 9 elementos preeechido a null
 
-function Board(){
+    const result = checkResult(piece); //guarda letra de vencedor ou empate
+
+    
+    function handleClick(i) {
+        if (piece[i] || checkResult(piece)) { //verifica se a peca ja foi preechida ou se ja ha uma vitoria
+            return;
+        }
+        
+        const nextPiece = piece.slice();
+        if(symbol === "X"){ //Se o simbolo atual for X o seguinte sera O
+            nextPiece[i] = "X";
+            setSymbol("O");
+        }
+        else{
+            nextPiece[i] = "O";
+            setSymbol("X");
+        }
+        
+        setPiece(nextPiece); 
+        
+    }
+    
     return(
-        <section className="board">
-              
-            <div className="board-row" id = "uno">
-                <div className="peca1">
-                    <BoardPiece value = ' '/>
+        <section className="board" >
+            {result == null ? ( // se nao existir vencedor ou empate mostra o tabuleiro normal
+                <div className="running">
+                    <div className="board-row" id = "uno" >
+                        <div className="peca1">
+                            <BoardPiece value = {piece[0]} onPieceClick={() =>handleClick(0)} />
+                        </div>  
+                        <div className="peca2">
+                            <BoardPiece value = {piece[1]} onPieceClick={() =>handleClick(1)} />
+                        </div>  
+                        <div className="peca3">
+                            <BoardPiece value = {piece[2]} onPieceClick={() =>handleClick(2)} />
+                        </div>  
+                    </div>
+                    <div className="board-row" id = "dos">
+                        <div className="peca1">
+                            <BoardPiece value = {piece[3]} onPieceClick={() =>handleClick(3)} />
+                        </div>  
+                        <div className="peca2">
+                            <BoardPiece value = {piece[4]} onPieceClick={() =>handleClick(4)} />
+                        </div>  
+                        <div className="peca3">
+                            <BoardPiece value = {piece[5]} onPieceClick={() =>handleClick(5)} />
+                        </div>    
+                    </div>
+                    <div className="board-row" id = "tres">
+                        <div className="peca1">
+                            <BoardPiece value = {piece[6]} onPieceClick={() =>handleClick(6)} />
+                        </div>  
+                        <div className="peca2">
+                            <BoardPiece value = {piece[7]} onPieceClick={() =>handleClick(7)} />
+                        </div>  
+                        <div className="peca3">
+                            <BoardPiece value = {piece[8]} onPieceClick={() =>handleClick(8)} />
+                        </div>   
+                    </div>
                 </div>  
-                <div className="peca2">
-                    <BoardPiece value = ' '/>
-                </div>  
-                <div className="peca3">
-                    <BoardPiece value = ' '/>
-                </div>  
-            </div>
-            <div className="board-row" id = "dos">
-                <div className="peca1">
-                    <BoardPiece value = ' '/>
-                </div>  
-                <div className="peca2">
-                    <BoardPiece value = ' '/>
-                </div>  
-                <div className="peca3">
-                    <BoardPiece value = ' '/>
-                </div>  
-            </div>
-            <div className="board-row" id = "tres">
-                <div className="peca1">
-                    <BoardPiece value = ' '/>
-                </div>  
-                <div className="peca2">
-                    <BoardPiece value = ' '/>
-                </div>  
-                <div className="peca3">
-                    <BoardPiece value = ' '/>
-                </div>  
-            </div>
+            )
+            : // else mostra letra vencedora, se empate mosta E
+                (<div className="tabOver" onMouseLeave ={() => letter(i,result)}>{result}</div> )
+            }
+            
+             
         </section>
     );
 }
