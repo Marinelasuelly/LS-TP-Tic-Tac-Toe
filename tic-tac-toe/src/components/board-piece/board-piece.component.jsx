5a894058aa8a56480461dc7047b5759e02a70a8c
ASofy0007 <103221068+ASofy0007@users.noreply.github.com>
2023-06-09 14:51:45 +0100

    Update board-piece.component.jsx

--- tic-tac-toe/src/components/board-piece/board-piece.component.jsx
+++ tic-tac-toe/src/components/board-piece/board-piece.component.jsx
@@ -3,12 +3,21 @@ import "./board-piece.css"
 
 import React from "react"
 
-function BoardPiece(){
+function BoardPiece({value, onPieceClick}){ //peca unica de um tabuleiro
+    
+    function isFull(){
+      if(value != null)
+        return true;
+      return false;
+    }
+
     return(
         <section id="boardPiece">
-              <button className="piece">X</button>
+              <button className="piece"  disabled = {isFull()} onClick = {onPieceClick}  > {value} </button>
         </section>
     );
+
+
 }
 
 export default BoardPiece
