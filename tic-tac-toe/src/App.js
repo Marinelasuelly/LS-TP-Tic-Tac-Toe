47074a416cbfdc38925d85e1488a50ca4ceb545f
ASofy0007 <103221068+ASofy0007@users.noreply.github.com>
2023-06-11 21:56:07 +0100

    Update App.js

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './assets/App.css';
 import {
   ControlPanel,
@@ -5,10 +6,31 @@ import {
 } from "./components";
 
 function App() {
+
+  const [gameStarted, setGameStarted] = useState(false);
+
+  const handleGameStart = () => {
+    if (gameStarted) {
+      console.log("Terminar");
+      setGameStarted(false);
+    } else {
+      console.log("Iniciar");
+      setGameStarted(true);
+    }
+
+    if (document.getElementById("vs").value === "vsUtilizador"){
+      let nome1 = prompt("Nome do primeiro jogador:");
+      let nome2 = prompt("Nome do segundo jogador:");
+    }
+    else{
+        let nome1 = prompt("Nome do jogador:");
+    }
+  };
+
   return (
     <div className="main-content">
-      <ControlPanel/>
-      <GamePanel/>
+      <ControlPanel onStarClick = {handleGameStart} gameStart = {gameStarted}/>
+      <GamePanel hide = {gameStarted? false : true}/> 
     </div>
   );
 }
