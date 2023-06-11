24c1f354c63ae01ff5573ff807715e8ac7e1d79e
ASofy0007 <103221068+ASofy0007@users.noreply.github.com>
2023-06-11 23:55:55 +0100

    Update modal-game-over.component.jsx

--- tic-tac-toe/src/components/modal-game-over/modal-game-over.component.jsx
+++ tic-tac-toe/src/components/modal-game-over/modal-game-over.component.jsx
@@ -3,8 +3,8 @@ import "./modal-game-over.css"
 
 import React from "react"
 
-function ModalGameOver({value, isHidden}){ //peca unica de um tabuleiro
-
+function ModalGameOver({value, isHidden, nome}){ //peca unica de um tabuleiro
+let winnerName; 
 const writeResult = ()  =>{
 
     if (value == null) 
@@ -12,6 +12,8 @@ const writeResult = ()  =>{
     if(value === "E"){
         return "Empate!"
     }else{
+        console.log(nome);
+        winnerName =`Parabéns ${(value == "X")? nome[0] : ((nome[1] == null)? "Computador" : nome[1])}!`; 
         return `Letra '${value}' venceu!`
     }
 
@@ -21,11 +23,11 @@ const writeResult = ()  =>{
         <section id="modal" className="modal" hidden = {isHidden}>
             <div className="message"> Fim de Jogo </div>
             <div className="messageWinner"> {writeResult()} </div>
+            <div className="messageName"> {winnerName} </div>
             <div className="btn">
                 <button className = "againbtn" > Jogar outra vez </button>
             </div>
-            
-   
+        
         </section>
     );
 
